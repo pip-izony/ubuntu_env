a20cc9e0200912d3bdc0f164719ff3442150020c
pip-izony <eeodqql09@gmail.com>
2024-12-20 22:09:23 -0500

    add: zsh-autosuggestions

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ sh install.sh
 rm install.sh
 echo "exec zsh" >> ~/.bashrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' $HOME/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/' $HOME/.zshrc
 
 # root environment
 sudo ln -s $HOME/.oh-my-zsh /root/.oh-my-zsh
